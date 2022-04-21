0373f9b8d678c9f47f47cffa8a223c83ccdfd6bf
ASHOK KUMAR <15051163+REGATTE@users.noreply.github.com>
2022-04-21 17:26:21 +0530

    Update px4_ws.sh

--- installation/px4_ws.sh
+++ installation/px4_ws.sh
@@ -6,6 +6,10 @@ echo "----- Build Workspace -----"
 mkdir -p ~/px4_ros_com_ros2/src
 git clone https://github.com/PX4/px4_ros_com.git ~/px4_ros_com_ros2/src/px4_ros_com
 git clone https://github.com/PX4/px4_msgs.git ~/px4_ros_com_ros2/src/px4_msgs
+
+cp ~/Documents/PX4-Fast-Planner/ROS2_WS/bspline ~/px4_ros_com_ros2/src
+cp ~/Documents/PX4-Fast-Planner/ROS2_WS/px4_fast_planner ~/px4_ros_com_ros2/src
+
 cd ~/px4_ros_com_ros2/src/px4_ros_com/scripts
 source clean_all.bash
 source build_ros2_workspace.bash
