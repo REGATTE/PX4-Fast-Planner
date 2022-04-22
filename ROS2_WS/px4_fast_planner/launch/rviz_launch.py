9d4d608881ca1ab2ccfd3d9aae9daafd26c39355
Ashok Kumar <ashok_a380@icloud.com>
2022-04-22 09:35:03 -0700

    updatted installation of rviz, fixed rviz launch file

--- ROS2_WS/px4_fast_planner/launch/rviz_launch.py
+++ ROS2_WS/px4_fast_planner/launch/rviz_launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             name="rvizvisualisation",
             package="rviz",
             executable='rviz',
-            output="log",
+            output="log"
         )
     })
 
