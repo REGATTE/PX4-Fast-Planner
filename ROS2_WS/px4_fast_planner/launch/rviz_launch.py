7e3591677ae6b9fb60ded57b190b7e500d1d26bb
Ashok Kumar <ashok_a380@icloud.com>
2022-04-22 09:35:03 -0700

    updatted installation of rviz, fixed rviz launch file

--- ROS2_WS/px4_fast_planner/launch/rviz_launch.py
+++ ROS2_WS/px4_fast_planner/launch/rviz_launch.py
@@ -5,14 +5,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription({
+
+    base_path = os.chdir("~/px4_ros_com_ros2/src/px4_fast_planner")
+    rviz_path = base_path + "/rviz/rviz.rviz"
+    ld = LaunchDescription()
+    ld.add_action(
         Node(
             name="rvizvisualisation",
-            package="rviz",
-            executable='rviz',
+            package="rviz2",
+            executable='rviz2',
             output="log",
-        )
-    })
+            arguments = ['-d', str(rviz_path)]
+        ),
+    )
 
-if __name__ == '__main__':
-    generate_launch_description()
+    return ld
